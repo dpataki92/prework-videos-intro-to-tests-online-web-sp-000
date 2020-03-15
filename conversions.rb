b46413aa4f5334bbcfb2edbcba6d1900bcada54e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-15 20:33:22 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -2,3 +2,9 @@ GRAMS_PER_OUNCE = 28.3495
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
+
+def meal_choice(arg1, arg2)
+  meal = 'Your choice for the dinner is #{arg1} and #{arg2}.'
+  puts meal
+  meal
+end
